c6bdd8e0564b562f1ac3af5b6ca7d8d958f1fa4f
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-29 15:48:34 +0530

    Update Areas.java

--- 29-09-22(LabDay1)/Areas.java
+++ 29-09-22(LabDay1)/Areas.java
@@ -3,7 +3,7 @@ package com.cognizant.shapes;
 public class Areas {
   public static void main(String[] args) {
 	Circle obj3= new Circle(10.0f,3.14f);
-	System.out.println("The area of the circle is "+obj3.calculateCircleArea(5));
-	System.out.println("The circumference of the circle is "+obj3.calculateCircumference(8));
+	System.out.println("The area of the circle is "+obj3.calculateCircleArea(3.8f));
+	System.out.println("The circumference of the circle is "+obj3.calculateCircumference(4.6f));
 }
 }

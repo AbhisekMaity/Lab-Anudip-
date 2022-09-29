c31af8ecd266d193fe05230423844045cbc0b73f
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-29 14:10:47 +0530

    Add files via upload

---
+++ Areas.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+public class Areas {
+  public static void main(String[] args) {
+	Circle obj3= new Circle(10.0f,3.14f);
+	System.out.println("The area of the circle is "+obj3.calculateCircleArea(5));
+	System.out.println("The circumference of the circle is "+obj3.calculateCircumference(8));
+}
+}
